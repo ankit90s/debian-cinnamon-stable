4f6feb4d1aad5a52613ce73418447777464405bb
ankit <ankit@localhost.localdomain>
2024-01-17 20:24:54 +0530

    updates

--- 2-apps.sh
+++ 2-apps.sh
@@ -57,7 +57,7 @@ rm -rf ani-cli
 curl -fSsL https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor | tee /usr/share/keyrings/google-chrome.gpg >> /dev/null
 echo deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main | tee /etc/apt/sources.list.d/google-chrome.list
 apt update
-apt install google-chrome-stable
+apt install -y google-chrome-stable
 
 # fast-cli
 echo -e "\e[1;32m Installing fast-cli for Internet speed test \e[0m"
